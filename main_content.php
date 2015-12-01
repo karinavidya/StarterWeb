a7b7c93f837237a7d2723800666328f79f055ebc
Karina <karinavidya@gmail.com>
2015-12-01 23:52:24 +0800

    [ADD] submit initial template

---
+++ main_content.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main_stylesheet.css" />
+	</head>
+	<!-- ************************************************ Body ****************************************************** -->
+	<body><h1>TEST</h1>
+		<!-- **************************************** Content ********************************************** -->
+		<div id="content" class="content_style">
+			<br>
+				<table>
+					<tr>
+						<th>HOME</th>
+						<th>ABOUT</th>
+						<th>CONTACT US</th>
+					</tr>
+				</table>
+			</br>
+		</div>		
+		<!-- **************************************** Footer *********************************************** -->
+	</body>
+</html>
