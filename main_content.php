ddb7ac432336647ad1ee1275a4ed9874a1d1e6d0
Karina <karinavidya@gmail.com>
2015-12-02 00:32:16 +0800

    [UPDATE] edit sample template code

--- main_content.php
+++ main_content.php
@@ -14,6 +14,36 @@
 						<th>ABOUT</th>
 						<th>CONTACT US</th>
 					</tr>
+					<tr>
+						<td>Data 1</td>
+						<td>Data 2</td>
+						<td>Data 3</td>
+					</tr>
+					<tr>
+						<td>Data 4</td>
+						<td>Data 5</td>
+						<td>Data 6</td>
+					</tr>
+					<tr>
+						<td>Data 7</td>
+						<td>Data 8</td>
+						<td>Data 9</td>
+					</tr>
+					<tr>
+						<td>Data 10</td>
+						<td>Data 11</td>
+						<td>Data 12</td>
+					</tr>
+					<tr>
+						<td>Data 13</td>
+						<td>Data 14</td>
+						<td>Data 15</td>
+					</tr>
+					<tr>
+						<td>Data 16</td>
+						<td>Data 17</td>
+						<td>Data 18</td>
+					</tr>
 				</table>
 			</br>
 		</div>		
